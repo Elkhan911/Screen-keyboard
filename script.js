0135522d979d93abe597015be92043116920aba7
Elkhan911 <955pro@mail.ru>
2022-08-20 20:39:20 +0500

    js - deleteBtn func , spaceBtn func, valid click for all buttons

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
-let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+const buttons = document.querySelectorAll(".buttons");
+const textarea1 = document.querySelector("#_textarea1");
+const span1 = document.querySelector("#span1");
+
+const spaceBtn = document.querySelector("#spaceButton");
+const enterBtn = document.querySelector("#enterButton");
+const capsLockBtn = document.querySelector("#capsLockButton");
+const deleteSymbBtn = document.querySelector("#deleteSymbol");
+const ClearBtn = document.querySelector("#clearAll");
+
+for (let button of buttons) {
+  button.addEventListener("click", function () {
+    textarea1.value += button.textContent;
+  });
+}
+
+spaceBtn.addEventListener("click", function () {
+  textarea1.value += " ";
+});
+
+enterBtn.addEventListener("click", function () {
+  textarea1.value += "\n";
+});
+
+deleteSymbBtn.addEventListener("click", function () {
+  let str = String(textarea1.value);
+  textarea1.value = str.slice(0, -1);
+});
+
+ClearBtn.addEventListener("click");
